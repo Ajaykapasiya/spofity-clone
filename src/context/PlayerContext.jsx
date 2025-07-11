4eeadee69a926174ead83db4e53a76cb003a5c48
ajay <ajaykumar290904@gmail.com>
2025-07-11 16:43:43 +0530

    feat: enhance DisplayAlbum with song list and update Navbar for navigation

---
+++ src/context/PlayerContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+export const PlayerContext = createContext();
+
+const PlayerContext = createContext();
+
+const PlayerContextProvider = () => {
+
+    const contextValue = {
+
+    }
+
+    return (
+        <PlayerContext.Provider value={contextValue}>
+            {props.children}
+        </PlayerContext.Provider>
+    )
+}
